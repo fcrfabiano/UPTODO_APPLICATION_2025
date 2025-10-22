55fcf87acc45a75f1dfb69d32efdfbe358ed3655
Fabiano César <fcfabiano.cesar@gmail.com>
2025-10-22 13:59:39 -0300

    chore: extend react native gesture handler types

--- nativewind-env.d.ts
+++ nativewind-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="nativewind/types" />
+
+import { ComponentType } from 'react';
+import { StyledComponent } from 'nativewind';
+
+declare module 'react-native-gesture-handler' {
+  // Isso adiciona suporte a className no RectButton e outros componentes do pacote
+  export const RectButton: StyledComponent<ComponentType<any>>;
+}
