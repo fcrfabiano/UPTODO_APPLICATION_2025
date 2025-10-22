86f325ef5f3e053f83215a103d63b87826251876
Fabiano César <fcfabiano.cesar@gmail.com>
2025-10-22 12:48:46 -0300

    styles: add new colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,8 +1,20 @@
 export const colors = {
     black: {
-        DEFAULT: '#000',
-        700: '#121212'
+        DEFAULT: '#000'
     },
-    gray: {},
-    support: {}
+    white: {
+        DEFAULT: '#fff'
+    },
+    gray: {
+        700: '#121212',
+        600: '#272727',
+        500: '#363636',
+        400: '#979797',
+        300: '#AFAFAF'
+    },
+    support: {
+        primary: '#8687E7',
+        error: '#FF4949',
+        warning: '#FFCC80'
+    }
 };
