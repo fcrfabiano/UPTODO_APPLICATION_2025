40d53965af1ea9647bb8ad0689c988b93518237c
Fabiano César <fcfabiano.cesar@gmail.com>
2025-10-22 12:49:06 -0300

    style: update input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput, View } from 'react-native';
 import { getClassNames } from '../utils/getClassNames';
+import { colors } from '../styles/colors';
 
+export const Input: React.FC<any> = ({ value, onChangeText, placeholder, multiline = false }) => {
+    const [isFocused, setIsFocused] = useState(false);
 
-export const Input: React.FC<any> = ({ value, onChangeText, placeholder, multiline = false }) => (
-    <View className="my-2">
-        <TextInput
-            value={value}
-            onChangeText={onChangeText}
-            placeholder={placeholder}
-            multiline={multiline}
-            className={getClassNames('border border-[#ddd] p-3 rounded-lg bg-white',
-                multiline && 'min-h-20'
-            )}
-        />
-    </View>
-);
+    function handleFocus() {
+        setIsFocused(true);
+    }
+
+    function handleBlur() {
+        setIsFocused(false);
+    }
+
+    return (
+        <View className="my-2">
+            <TextInput
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+                value={value}
+                onChangeText={onChangeText}
+                placeholder={placeholder}
+                multiline={multiline}
+                placeholderTextColor={isFocused ? colors.white.DEFAULT : colors.gray[300]}
+                placeholderClassName="text-gray-300 focus:text-white"
+                className={getClassNames('border border-transparent focus:border-gray-400 p-3 rounded-lg bg-gray-700 focus:bg-gray-500 text-white font-sans-regular text-lg',
+                    multiline && 'min-h-40 align-top'
+                )}
+            />
+        </View>
+    );
+}
